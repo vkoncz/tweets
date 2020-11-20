61ed2233d1ead073bc101f07ff74a6a69ad935a0
Viktor Koncz <konczviktor@gmail.com>
2020-11-20 22:10:28 +0100

    2.2 Add test for get tweets route

--- tweets-server/src/app.test.ts
+++
@@ -1,8 +0,0 @@
-import { getMessage } from './app';
-
-it('should return with message', () => {
-  const message = getMessage();
-
-  expect(typeof message).toBe('string');
-  expect(message).toBe("It's working");
-});
